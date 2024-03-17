60f03f09da71a6aed26d87f87a3226a89ddb4d87
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-03-17 19:55:14 +0300

    minor changes and dump function

--- instruction.hpp
+++ instruction.hpp
@@ -4,18 +4,28 @@
 
 namespace rv32i_model {
 
-class inst_t final {
-    int opcode;
+class inst_d final {
+    int opc;
     int rd;
     int rs1;
     int rs2;
     int funct3;
     int funct7;
 
-    friend class Processor;
+    friend class Executer;
 public:
-    inst_t(int op, int rd_val, int rs1_val, int rs2_val, int func3_val, int func7_val)
-        : opcode(op), rd(rd_val), rs1(rs1_val), rs2(rs2_val), funct3(func3_val), funct7(func7_val) {}
+    inst_d(int op, int rd_val, int rs1_val, int rs2_val, int func3_val, int func7_val)
+        : opc(op), rd(rd_val), rs1(rs1_val), rs2(rs2_val), funct3(func3_val), funct7(func7_val) {}
+
+    void dump() {
+        std::cout << std::hex << opc << std::endl;
+        std::cout << std::hex << rd << std::endl;
+        std::cout << std::hex << rs1 << std::endl;
+        std::cout << std::hex << rs2 << std::endl;
+        std::cout << std::hex << funct3 << std::endl;
+        std::cout << std::hex << funct7 << std::endl;
+    }
+
 };
 
 }
