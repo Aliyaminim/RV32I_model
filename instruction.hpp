c172b13c3ca8296bf35c2e016453515460263ebd
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-03-17 22:56:52 +0300

    changed types to unsigned int

--- instruction.hpp
+++ instruction.hpp
@@ -5,16 +5,16 @@
 namespace rv32i_model {
 
 class inst_d final {
-    int opc;
-    int rd;
-    int rs1;
-    int rs2;
-    int funct3;
-    int funct7;
+    unsigned int opc;
+    unsigned int rd;
+    unsigned int rs1;
+    unsigned int rs2;
+    unsigned int funct3;
+    unsigned int funct7;
 
-    friend class Executer;
+    friend class Processor;
 public:
-    inst_d(int op, int rd_val, int rs1_val, int rs2_val, int func3_val, int func7_val)
+    inst_d(unsigned int op, unsigned int rd_val, unsigned int rs1_val, unsigned int rs2_val, unsigned int func3_val, unsigned int func7_val)
         : opc(op), rd(rd_val), rs1(rs1_val), rs2(rs2_val), funct3(func3_val), funct7(func7_val) {}
 
     void dump() {
