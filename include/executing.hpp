97b657b890f3c4816877f8442e5c841d7a7f0009
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-03-28 12:12:39 +0300

    made as many values as possible unsigned, added slti, sltu instructions

--- include/executing.hpp
+++ include/executing.hpp
@@ -5,8 +5,10 @@
 
 namespace rv32i_model {
 
-enum class Opcode : unsigned int {
+enum class Opcode : uint32_t {
     OP_IMM = 0x13,
+    LUI = 0x37,
+    AUIPC = 17,
     OP = 0x33,
     JAL = 0x6F,
     JALR = 0x67,
@@ -17,20 +19,20 @@ enum class Opcode : unsigned int {
     FENCE = 0xF
 };
 
-enum class Funct3 : unsigned int {
-    VAR_1 = 0x0,
-    VAR_2 = 0x1,
-    VAR_3 = 0x2,
-    VAR_4 = 0x3,
-    VAR_5 = 0x4,
-    VAR_6 = 0x5,
-    VAR_7 = 0x6,
-    VAR_8 = 0x7
+enum class Funct3 : uint32_t {
+    VAR_0 = 0x0,
+    VAR_1 = 0x1,
+    VAR_2 = 0x2,
+    VAR_3 = 0x3,
+    VAR_4 = 0x4,
+    VAR_5 = 0x5,
+    VAR_6 = 0x6,
+    VAR_7 = 0x7
 };
 
-enum class Funct7 : unsigned int {
-    VAR_1 = 0x0,
-    VAR_2 = 0x20
+enum class Funct7 : uint32_t {
+    VAR_0 = 0x0,
+    VAR_1 = 0x20
 };
 
 void Processor::execute(inst_d dec_inst) {
@@ -39,6 +41,10 @@ void Processor::execute(inst_d dec_inst) {
         case Opcode::OP_IMM:
             execute_op_imm(funct7, rs2, rs1, funct3, rd);
             break;
+        case Opcode::LUI:
+            //execute_lui(dec_inst);
+        case Opcode::AUIPC:
+            //execute_auipc(dec_inst);
         case Opcode::OP:
             execute_op(funct7, rs2, rs1, funct3, rd);
             break;
@@ -68,15 +74,24 @@ void Processor::execute(inst_d dec_inst) {
     }
 }
 
-void Processor::execute_op_imm(unsigned int funct7, unsigned int rs2, \
-                            unsigned int rs1, unsigned int funct3, unsigned int rd) {
-    unsigned int imm;
+void Processor::execute_op_imm(uint32_t funct7, uint32_t rs2, \
+                            uint32_t rs1, uint32_t funct3, uint32_t rd) {
+    uint32_t imm_tmp = 0u;
+    imm_tmp += (funct7 << 25) + (rs2 << 20);
+    int32_t imm = (int32_t) imm_tmp;
+    imm = imm >> 20;
+
     switch (static_cast<Funct3>(funct3)) {
-        case Funct3::VAR_1: //addi
+        case Funct3::VAR_0: //addi
             std::cout << "ADDI" << std::endl;
-            imm = (funct7 << 5) + rs2;
             regfile.write(rd, imm + regfile.read(rs1));
             break;
+        case Funct3::VAR_2: //slti
+            std::cout << "SLTI" << std::endl;
+            regfile.write(rd, ((int32_t)regfile.read(rs1) < (int32_t)imm));
+        case Funct3::VAR_3: //sltiu
+            std::cout << "SLTIU" << std::endl;
+            regfile.write(rd, ((uint32_t)regfile.read(rs1) < (uint32_t)imm));
         default:
             throw std::runtime_error ("Unknown instruction");
             break;
@@ -84,16 +99,16 @@ void Processor::execute_op_imm(unsigned int funct7, unsigned int rs2, \
 }
 
 
-void Processor::execute_op(unsigned int funct7, unsigned int rs2, \
-                            unsigned int rs1, unsigned int funct3, unsigned int rd) {
+void Processor::execute_op(uint32_t funct7, uint32_t rs2, \
+                            uint32_t rs1, uint32_t funct3, uint32_t rd) {
     switch (static_cast<Funct3>(funct3)) {
         case Funct3::VAR_1:
             switch (static_cast<Funct7>(funct7)) {
-                case Funct7::VAR_1: //add
+                case Funct7::VAR_0: //add
                     std::cout << "ADD" << std::endl;
                     regfile.write(rd, regfile.read(rs1) + regfile.read(rs2));
                     break;
-                case Funct7::VAR_2: //sub
+                case Funct7::VAR_1: //sub
                     std::cout << "SUB" << std::endl;
                     regfile.write(rd, regfile.read(rs1) - regfile.read(rs2));
                     break;
@@ -108,14 +123,14 @@ void Processor::execute_op(unsigned int funct7, unsigned int rs2, \
     }
 }
 
-void Processor::execute_jal(unsigned int funct7, unsigned int rs2, \
-                            unsigned int rs1, unsigned int funct3, unsigned int rd) {
+void Processor::execute_jal(uint32_t funct7, uint32_t rs2, \
+                            uint32_t rs1, uint32_t funct3, uint32_t rd) {
     std::cout << "JAL" << std::endl;
 
 }
 
-void Processor::execute_jalr(unsigned int funct7, unsigned int rs2, \
-                            unsigned int rs1, unsigned int funct3, unsigned int rd) {
+void Processor::execute_jalr(uint32_t funct7, uint32_t rs2, \
+                            uint32_t rs1, uint32_t funct3, uint32_t rd) {
     std::cout << "JALR" << std::endl;
     #ifdef DEBUG
     int32_t imm = ((int)(memory[PC-1] & 0xFFF00000)) >> 20;
@@ -125,30 +140,30 @@ void Processor::execute_jalr(unsigned int funct7, unsigned int rs2, \
 }
 
 
-void Processor::execute_branch(unsigned int funct7, unsigned int rs2, \
-                            unsigned int rs1, unsigned int funct3, unsigned int rd) {
+void Processor::execute_branch(uint32_t funct7, uint32_t rs2, \
+                            uint32_t rs1, uint32_t funct3, uint32_t rd) {
 
 }
 
 
-void Processor::execute_load(unsigned int funct7, unsigned int rs2, \
-                            unsigned int rs1, unsigned int funct3, unsigned int rd) {
+void Processor::execute_load(uint32_t funct7, uint32_t rs2, \
+                            uint32_t rs1, uint32_t funct3, uint32_t rd) {
 
 }
 
 
-void Processor::execute_store(unsigned int funct7, unsigned int rs2, \
-                            unsigned int rs1, unsigned int funct3, unsigned int rd) {
+void Processor::execute_store(uint32_t funct7, uint32_t rs2, \
+                            uint32_t rs1, uint32_t funct3, uint32_t rd) {
 
 }
 
-void Processor::execute_system(unsigned int funct7, unsigned int rs2, \
-                            unsigned int rs1, unsigned int funct3, unsigned int rd) {
+void Processor::execute_system(uint32_t funct7, uint32_t rs2, \
+                            uint32_t rs1, uint32_t funct3, uint32_t rd) {
 
 }
 
-void Processor::execute_fence(unsigned int funct7, unsigned int rs2, \
-                            unsigned int rs1, unsigned int funct3, unsigned int rd) {
+void Processor::execute_fence(uint32_t funct7, uint32_t rs2, \
+                            uint32_t rs1, uint32_t funct3, uint32_t rd) {
 
 }
 
