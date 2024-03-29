1c8679bf81de754a1921b69e83bb4972414bfbe3
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-03-29 12:09:48 +0300

    made new representation of instructions using inheritance

--- include/instruction.hpp
+++ include/instruction.hpp
@@ -4,7 +4,13 @@
 
 namespace rv32i_model {
 
-class inst_d final {
+class instD {
+public:
+    virtual ~instD() = default;
+};
+
+//только по типу можно
+class instD_R : public instD {
     uint32_t opc;
     uint32_t rd;
     uint32_t rs1;
@@ -14,18 +20,112 @@ class inst_d final {
 
     friend class Processor;
 public:
-    inst_d(uint32_t op, uint32_t rd_val, uint32_t rs1_val, uint32_t rs2_val, uint32_t func3_val, uint32_t func7_val)
-        : opc(op), rd(rd_val), rs1(rs1_val), rs2(rs2_val), funct3(func3_val), funct7(func7_val) {}
-
-    void dump() {
-        std::cout << std::hex << opc << std::endl;
-        std::cout << std::hex << rd << std::endl;
-        std::cout << std::hex << rs1 << std::endl;
-        std::cout << std::hex << rs2 << std::endl;
-        std::cout << std::hex << funct3 << std::endl;
-        std::cout << std::hex << funct7 << std::endl;
+    explicit instD_R(uint32_t& inst) {
+        opc = inst & 0x7F;
+        rd = (inst >> 7) & 0x1F;
+        rs1 = (inst >> 15) & 0x1F;
+        rs2 = (inst >> 20) & 0x1F;
+        funct3 = (inst >> 12) & 0x7;
+        funct7 = (inst >> 25) & 0x7F;
     }
 
+    ~instD_R() = default;
+};
+
+class instD_I : public instD {
+    uint32_t opc;
+    uint32_t rd;
+    uint32_t rs1;
+    uint32_t funct3;
+    int32_t imm;
+
+    friend class Processor;
+public:
+    explicit instD_I(uint32_t& inst) {
+        opc = inst & 0x7F;
+        rd = (inst >> 7) & 0x1F;
+        rs1 = (inst >> 15) & 0x1F;
+        funct3 = (inst >> 12) & 0x7;
+        imm = (int32_t)(((int32_t) inst) >> 20);
+    }
+
+    ~instD_I() = default;
+};
+
+class instD_S : public instD {
+    uint32_t opc;
+    uint32_t rs1;
+    uint32_t rs2;
+    uint32_t funct3;
+    int32_t imm;
+
+    friend class Processor;
+public:
+    explicit instD_S(uint32_t& inst) {
+        opc = inst & 0x7F;
+        rs1 = (inst >> 15) & 0x1F;
+        rs2 = (inst >> 20) & 0x1F;
+        funct3 = (inst >> 12) & 0x7;
+        uint32_t imm_tmp = ((inst >> 7) & 0x1F) + ((((int32_t)inst) >> 25) << 5);
+        imm = (int32_t)imm_tmp;
+    }
+    ~instD_S() = default;
+};
+
+class instD_B : public instD {
+    uint32_t opc;
+    uint32_t rs1;
+    uint32_t rs2;
+    uint32_t funct3;
+    int32_t imm;
+
+    friend class Processor;
+public:
+    explicit instD_B(uint32_t& inst) {
+        opc = inst & 0x7F;
+        rs1 = (inst >> 15) & 0x1F;
+        rs2 = (inst >> 20) & 0x1F;
+        funct3 = (inst >> 12) & 0x7;
+        uint32_t imm_tmp = ((inst & 0x80000000) ? 0xFFFFE000 : 0x0);
+        imm_tmp += ((inst >> 7) & 0x1) << 11 + ((inst >> 8) & 0xF) << 1;
+        imm_tmp += ((inst >> 25) & 0x3F) << 5 + ((inst >> 31) & 0x1) << 12;
+        imm = (int32_t)imm_tmp;
+    }
+    ~instD_B() = default;
+};
+
+class instD_U : public instD {
+    uint32_t opc;
+    uint32_t rd;
+    int32_t imm;
+
+    friend class Processor;
+public:
+    explicit instD_U(uint32_t& inst) {
+        opc = inst & 0x7F;
+        rd = (inst >> 7) & 0x1F;
+        imm = (int32_t)(inst & 0xFFFFF000);
+    }
+    ~instD_U() = default;
+};
+
+class instD_J : public instD {
+    uint32_t opc;
+    uint32_t rd;
+    int32_t imm;
+
+    friend class Processor;
+public:
+    explicit instD_J(uint32_t& inst) {
+        opc = inst & 0x7F;
+        rd = (inst >> 7) & 0x1F;
+        uint32_t imm_tmp = ((inst & 0x80000000) ? 0xFFE00000 : 0x0);
+        imm_tmp += (((inst >> 12) & 0xFF) << 12)+ (((inst >> 20) & 0x1) << 11);
+        imm_tmp += ((inst >> 21) & 0x3FF) << 1;
+        imm_tmp += ((inst >> 31) & 0x1) << 20;
+        imm = (int32_t)imm_tmp;
+    }
+    ~instD_J() = default;
 };
 
 }
