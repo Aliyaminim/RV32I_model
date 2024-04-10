4a2bbe9fb702157b4a14d35217e1dbc051525181
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-04-10 22:32:04 +0300

    added draft of processing system instructions

--- include/executing.hpp
+++ include/executing.hpp
@@ -103,30 +103,30 @@ void Processor::execute_op_imm(instD* dec_inst) {
 
     switch (static_cast<Funct3>(funct3)) {
         case Funct3::VAR_0: //addi or nop
-            std::cout << "ADDI" << std::endl;
+            logstream << "ADDI" << std::endl;
             if ((rs1 == 0) && (rd == 0) && (imm == 0))
                 increase_PC(); //nop
             else
                 regfile.write(rd, imm + regfile.read(rs1));
             break;
         case Funct3::VAR_2: //slti
-            std::cout << "SLTI" << std::endl;
+            logstream << "SLTI" << std::endl;
             regfile.write(rd, ((int32_t)regfile.read(rs1) < (int32_t)imm));
             break;
         case Funct3::VAR_3: //sltiu
-            std::cout << "SLTIU" << std::endl;
+            logstream << "SLTIU" << std::endl;
             regfile.write(rd, ((uint32_t)regfile.read(rs1) < (uint32_t)imm));
             break;
         case Funct3::VAR_4: //xori
-            std::cout << "XORI" << std::endl;
+            logstream << "XORI" << std::endl;
             regfile.write(rd, ((uint32_t)regfile.read(rs1) ^ (uint32_t)imm));
             break;
         case Funct3::VAR_6: //ori
-            std::cout << "ORI" << std::endl;
+            logstream << "ORI" << std::endl;
             regfile.write(rd, ((uint32_t)regfile.read(rs1) | (uint32_t)imm));
             break;
         case Funct3::VAR_7: //andi
-            std::cout << "ANDI" << std::endl;
+            logstream << "ANDI" << std::endl;
             regfile.write(rd, ((uint32_t)regfile.read(rs1) & (uint32_t)imm));
             break;
         default:
@@ -136,11 +136,13 @@ void Processor::execute_op_imm(instD* dec_inst) {
 }
 
 void Processor::execute_lui(instD* dec_inst) {
+    logstream << "LUI" << std::endl;
     auto[opc, rd, imm] = *dynamic_cast<instD_U*>(dec_inst);
     regfile.write(rd, imm);
 }
 
 void Processor::execute_auipc(instD* dec_inst) {
+    logstream << "AUIPC" << std::endl;
     auto[opc, rd, imm] = *dynamic_cast<instD_U*>(dec_inst);
     imm += (int32_t)get_PC();
     regfile.write(rd, imm);
@@ -154,11 +156,11 @@ void Processor::execute_op(instD* dec_inst) {
         case Funct3::VAR_0:
             switch (static_cast<Funct7>(funct7)) {
                 case Funct7::VAR_0: //ADD
-                    std::cout << "ADD" << std::endl;
+                    logstream << "ADD" << std::endl;
                     regfile.write(rd, regfile.read(rs1) + regfile.read(rs2));
                     break;
                 case Funct7::VAR_1: //SUB
-                    std::cout << "SUB" << std::endl;
+                    logstream << "SUB" << std::endl;
                     regfile.write(rd, regfile.read(rs1) - regfile.read(rs2));
                     break;
                 default:
@@ -167,12 +169,12 @@ void Processor::execute_op(instD* dec_inst) {
             }
             break;
         case Funct3::VAR_2: //slt
-            std::cout << "SLT" << std::endl;
+            logstream << "SLT" << std::endl;
             tmp_res = ((int32_t)regfile.read(rs1) < (int32_t)regfile.read(rs2)) ? 1u : 0;
             regfile.write(rd, tmp_res);
             break;
         case Funct3::VAR_3: //sltu
-            std::cout << "SLTU" << std::endl;
+            logstream << "SLTU" << std::endl;
             if (rs1 != 0)
                 tmp_res = ((uint32_t)regfile.read(rs1) < (uint32_t)regfile.read(rs2)) ? 1u : 0;
             else
@@ -181,31 +183,31 @@ void Processor::execute_op(instD* dec_inst) {
             regfile.write(rd, tmp_res);
             break;
         case Funct3::VAR_7: //add
-            std::cout << "AND" << std::endl;
+            logstream << "AND" << std::endl;
             regfile.write(rd, ((uint32_t)regfile.read(rs1) & (uint32_t)regfile.read(rs2)));
             break;
         case Funct3::VAR_6: //or
-            std::cout << "OR" << std::endl;
+            logstream << "OR" << std::endl;
             regfile.write(rd, ((uint32_t)regfile.read(rs1) | (uint32_t)regfile.read(rs2)));
             break;
         case Funct3::VAR_4: //xor
-            std::cout << "XOR" << std::endl;
+            logstream << "XOR" << std::endl;
             regfile.write(rd, ((uint32_t)regfile.read(rs1) ^ (uint32_t)regfile.read(rs2)));
             break;
         case Funct3::VAR_1: //sll
-            std::cout << "SLL" << std::endl;
+            logstream << "SLL" << std::endl;
             tmp_res = regfile.read(rs1) << regfile.read(rs2);
             regfile.write(rd, tmp_res);
             break;
         case Funct3::VAR_5:
             switch (static_cast<Funct7>(funct7)) {
                 case Funct7::VAR_0: //srl
-                    std::cout << "SRL" << std::endl;
+                    logstream << "SRL" << std::endl;
                     tmp_res = ((uint32_t)regfile.read(rs1)) >> regfile.read(rs2);
                     regfile.write(rd, tmp_res);
                     break;
                 case Funct7::VAR_1: //sra
-                    std::cout << "SRA" << std::endl;
+                    logstream << "SRA" << std::endl;
                     tmp_res = ((int32_t)regfile.read(rs1)) >> regfile.read(rs2);
                     regfile.write(rd, tmp_res);
                     break;
@@ -218,41 +220,38 @@ void Processor::execute_op(instD* dec_inst) {
 }
 
 void Processor::execute_jal(instD* dec_inst) {
-    std::cout << "JAL" << std::endl;
+    logstream << "JAL" << std::endl;
     auto[opc, rd, imm] = *dynamic_cast<instD_J*>(dec_inst);
-    std::size_t imm_2index = imm /sizeof(uint32_t);
-    regfile.write(rd, get_PC() + 1);
+    regfile.write(rd, get_PC() + 4);
 
     if ( !(imm & 0x3) ) {
         // The address is 4-byte aligned here
-        increase_PC(imm_2index);
+        increase_PC(imm);
     } else
         throw std::runtime_error ("An instruction-address-misaligned exception");
 }
 
 void Processor::execute_jalr(instD* dec_inst) {
-    std::cout << "JALR" << std::endl;
+    logstream << "JALR" << std::endl;
     auto[opc, rd, rs1, funct3, imm] = *dynamic_cast<instD_I*>(dec_inst);
 
     int32_t new_address = regfile.read(rs1) + imm;
-    std::size_t new_address_2index = new_address /sizeof(uint32_t);
 
     if (rd != 0) //sometimes dest is not required
-        regfile.write(rd, get_PC() + 1);
+        regfile.write(rd, get_PC() + 4);
 
     if ( !(new_address & 0x3) ) {
         // The address is 4-byte aligned here
-        set_PC(new_address_2index);
+        set_PC(new_address);
     } else
         throw std::runtime_error ("An instruction-address-misaligned exception");
 }
 
 
 int Processor::execute_branch(instD* dec_inst) {
-    std::cout << "BRANCH" << std::endl;
+    logstream << "BRANCH" << std::endl;
     auto[opc, rs1, rs2, funct3, imm] = *dynamic_cast<instD_B*>(dec_inst);
     bool branch_flag = false;
-    std::size_t imm_2index = imm /sizeof(uint32_t);
 
     switch(static_cast<Funct3>(funct3)) {
         case Funct3::VAR_0: //beq
@@ -287,7 +286,7 @@ int Processor::execute_branch(instD* dec_inst) {
     if (branch_flag) {
         if ( !(imm & 0x3) ) {
             // The address is 4-byte aligned here
-            increase_PC(imm_2index);
+            increase_PC(imm);
         } else
             throw std::runtime_error ("An instruction-address-misaligned exception");
     }
@@ -297,7 +296,7 @@ int Processor::execute_branch(instD* dec_inst) {
 
 
 void Processor::execute_load(instD* dec_inst) {
-    std::cout << "LOAD" << std::endl;
+    logstream << "LOAD" << std::endl;
     auto[opc, rd, rs1, funct3, imm] = *dynamic_cast<instD_I*>(dec_inst);
 
     int32_t effective_address = regfile.read(rs1) + imm;
@@ -333,7 +332,7 @@ void Processor::execute_load(instD* dec_inst) {
 
 
 void Processor::execute_store(instD* dec_inst) {
-    std::cout << "STORE" << std::endl;
+    logstream << "STORE" << std::endl;
     auto[opc, rs1, rs2, funct3, imm] = *dynamic_cast<instD_S*>(dec_inst);
 
     int32_t effective_address = regfile.read(rs1) + imm;
@@ -357,40 +356,48 @@ void Processor::execute_store(instD* dec_inst) {
 }
 
 void Processor::execute_system(instD* dec_inst) {
-    std::cout << "SYSTEM" << std::endl;
+    logstream << "SYSTEM" << std::endl;
+    auto[opc, rd, rs1, funct3, imm] = *dynamic_cast<instD_I*>(dec_inst);
+
     enum {
         syscall_READ = 63,
         syscall_WRITE = 64,
         syscall_EXIT = 93
     };
 
-    auto syscall_type = regfile.read(17);
+    if (imm == 0) { //ecall
+        auto syscall_type = regfile.read(17);
 
-    int32_t fd = regfile.read(10);
-    int32_t status = fd;
-    int32_t buf_model = regfile.read(11);
-    int32_t size = regfile.read(12);
-    void* buf = static_cast<char*>(memory.begin()) + buf_model;
+        int32_t fd = regfile.read(10);
+        int32_t status = fd;
+        int32_t buf_model = regfile.read(11);
+        int32_t size = regfile.read(12);
+        char* buf = static_cast<char*>(memory.begin_ptr()) + buf_model;
 
-    switch (syscall_type) {
-        case syscall_READ:
-            read(fd, buf, size);
-            break;
-        case syscall_WRITE:
-            write(fd, buf, size);
-            break;
-        case syscall_EXIT:
-            exit(status);
-            break;
-        default:
-            throw std::runtime_error ("Unknown system call");
-            break;
-    }
+        switch (syscall_type) {
+            case syscall_READ:
+                read(fd, buf, size);
+                break;
+            case syscall_WRITE:
+                write(fd, buf, size);
+                break;
+            case syscall_EXIT:
+                //smth happens
+                exit(status);
+                break;
+            default:
+                throw std::runtime_error ("Unknown system call");
+                break;
+        }
+    } else if (imm == 1) { //ebreak
+        //smth happens
+    } else
+        throw std::runtime_error ("Unknown instruction");
 
 }
 
-void Processor::execute_fence(instD* dec_inst) {
-    std::cout << "FENCE" << std::endl;
+void Processor::execute_fence(instD* dec_inst) { //? nop
+    logstream << "FENCE" << std::endl;
     throw std::runtime_error ("Unknown instruction");
 }
 
