2ea5f70896dc6c31076482fb0b69372a73dd80c1
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-04-10 20:30:41 +0300

    changed memory image getting routine and added logfile to report about processed instructions and dump memory image there

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,58 @@
 #include <iostream>
-#include "rv32i_model.hpp"
+#include <fstream>
 #include <vector>
+#include <endian.h>
+#include <filesystem>
+#include <string>
+#include "rv32i_model.hpp"
 
-auto read_input(std::istream& is) {
-    std::vector<uint32_t> input;
-    uint32_t inst;
+std::vector<uint32_t> get_memory_image(const char * filename) {
+    if (!std::filesystem::is_regular_file(filename))
+        throw std::runtime_error (std::string("ERROR:\nCan't open file ") + std::string(filename));
 
-    while(is >> std::hex >> inst, is.good())
-        input.push_back(inst);
+    std::ifstream binary_input{filename, std::ios::binary};
+    if (!binary_input.is_open())
+        throw std::runtime_error (std::string("ERROR:\nCan't open file ") + std::string(filename));
 
-    return input;
+    std::vector<uint32_t> memory_image;
+    uint32_t n;
+    while(binary_input.read((char*)&n, sizeof(n))) {
+        memory_image.push_back(le32toh(n));
+        n = 0;
+    }
+
+    if (binary_input.gcount() > 0)
+        memory_image.push_back(le32toh(n));
+
+    return memory_image;
 }
 
-int main() {
-    auto in = read_input(std::cin);
 
-    rv32i_model::Processor pr(64);
-    pr.load_input_to_memory(in);
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "ERROR:\nPlease enter: ./main <file>" << std::endl;
+        return 1;
+    }
 
-    pr.process();
+    try {
+        std::size_t mem_size = (1ull << 32) - 1; //RV32I provides a 32-bit address space
+        std::ofstream logstream("logfile.out");
+        rv32i_model::Processor model_rv32i(mem_size, logstream);
+
+        std::vector<uint32_t> memory_image = get_memory_image(argv[1]);
+        model_rv32i.load_input_to_memory(memory_image);
+
+        #ifdef DEBUG
+        model_rv32i.dump_memory();
+        #endif
+
+        model_rv32i.process();
+
+    }
+    catch(const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
