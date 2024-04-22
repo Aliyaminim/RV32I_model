f49eea0d85ac9ed4939c901d99ee7245bfb71fa5
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-04-22 23:56:16 +0300

    small changes

--- include/regfile.hpp
+++ include/regfile.hpp
@@ -7,14 +7,14 @@
 namespace rv32i_model {
 
 class Regfile final {
-/*  reg[0] = 0 (always)
-    reg[1] - return address for a call
-    reg[5] - alternate link register
-    reg[2] - stack pointer
+/*  regs[0] = 0 (always)
+    regs[1] - return address for a call
+    regs[5] - alternate link register
+    regs[2] - stack pointer
 */
-    std::array<int32_t,32> regs;
+    std::array<int32_t, 32> regs;
 public:
-    Regfile() { regs.fill(0); }
+    Regfile() : regs{} {}
 
     int32_t read(std::size_t i) const { return regs[i]; }
 
@@ -24,7 +24,7 @@ public:
     }
 
     void dump() const {
-        for (auto && s : regs)
+        for (const int32_t& s : regs)
             std::cout << s << std::endl;
     }
 
