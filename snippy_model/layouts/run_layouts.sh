f311f3fc9cf3f3c4012c25e89b10cc8e9643f19b
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-05-13 09:39:57 +0300

    changed logfile's name

--- snippy_model/layouts/run_layouts.sh
+++ snippy_model/layouts/run_layouts.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mkdir -p bin
+
 for file in ./*.yaml; do
     echo -e "\e[34m$file\e[0m"
-    llvm-snippy $file -o ./bin/
+    llvm-snippy $file -o ./bin/${file%.yaml}
     echo
 done
